ff1c1e88eb766020a51e322d0a1d8cec09173538
oushiming <oushiming@sxt.com>
2018-04-27 23:49:31 +0800

    init

---
+++ src/main/java/com/hmily/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.hmily.dao;
+
+import com.hmily.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
