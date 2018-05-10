2922ca3d3721375a643c836775dbe406dcc20c0c
oushiming <oushiming@sxt.com>
2018-05-10 20:43:00 +0800

    userController

--- src/main/java/com/hmily/controller/portal/UserController.java
+++ src/main/java/com/hmily/controller/portal/UserController.java
@@ -1,11 +1,11 @@
 package com.hmily.controller.portal;
 
 import com.hmily.common.Const;
+import com.hmily.common.ResponseCode;
 import com.hmily.common.ServerResponse;
 import com.hmily.pojo.User;
 import com.hmily.service.IUserService;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -54,4 +54,77 @@ public class UserController {
         return iUserService.checkValid(str, type);
     }
 
+    @RequestMapping(value = "logout.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> logout(HttpSession session){
+        session.removeAttribute(Const.CURRENT_USER);
+        return ServerResponse.createBySuccessMessage("退出成功");
+    }
+
+    @RequestMapping(value = "get_user_info", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<User> getUserInfo(HttpSession session){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(null == user){
+            return ServerResponse.createByErrorMessage("当前用户未登录");
+        }
+        return ServerResponse.createBySuccess(user);
+    }
+
+    @RequestMapping(value = "forget_get_question", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> forgetGetQuestion(String username){
+        return iUserService.forgetGetQuestion(username);
+    }
+
+    @RequestMapping(value = "forget_check_answer", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> forgetCheckAnswer(String username, String question, String answer){
+        return iUserService.checkAnswer(username, question, answer);
+    }
+
+    @RequestMapping(value = "forget_rest_password", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> forgetRestPassword(String username, String passwordNew, String forgetToken){
+        return iUserService.forgetResetPassword(username, passwordNew, forgetToken);
+    }
+
+    @RequestMapping(value = "reset_password.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> resetPassword(HttpSession session, String passwordOld, String passwordNew){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(null == user){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录");
+        }
+        return iUserService.resetPassword(passwordOld, passwordNew, user);
+    }
+
+    @RequestMapping(value = "update_information.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> updateInformation(HttpSession session, User user){
+        User currentUser = (User)session.getAttribute(Const.CURRENT_USER);
+        if(currentUser == null){
+            return ServerResponse.createByErrorMessage("用户未登录");
+        }
+        user.setId(currentUser.getId());
+        user.setUsername(currentUser.getUsername());
+        user.setPassword(null);
+        user.setRole(Const.Role.ROLE_CUSTOMER);
+        ServerResponse<User> res = iUserService.updateInformation(user);
+        if(res.isSuccess()){
+            session.setAttribute(Const.CURRENT_USER, res.getData());
+        }
+        return res;
+    }
+
+
+    @RequestMapping(value = "get_information.do", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<User> getInformation(HttpSession session){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(null == user){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录");
+        }
+        return ServerResponse.createBySuccess(user);
+    }
 }
